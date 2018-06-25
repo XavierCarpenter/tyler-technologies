58a60e855c3f2ec510524b21b57e15eba095e9d1
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-06-24 22:32:03 -0500

    styling for larger screens

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import logo from "../../images/tyler-logo-header.png";
 import './Header.css';
 import { Link, withRouter } from 'react-router-dom'
@@ -18,34 +18,36 @@ class Header extends Component {
         return (
             <div className="header-container">
                 <header>
-                    <div className="logo-container"><img className="logo" src={logo} /></div>
-                    <div className="login-register"><h4>Login</h4> | <h4>Register</h4></div>
+                    <div className="top-nav">
+                        <div className="logo-container"><img className="logo" src={logo} /></div>
+                        <div className="hamburger search"><i className="fas fa-search" /></div>
+                        <div className="login-register"><h4>Login</h4> | <h4>Register</h4></div>
+                        {this.state.navActive === false ? <div className="hamburger bars"><i onClick={() => this.changeNav()} className="fas fa-bars" /></div> :
+                            <div className="hamburger"><i onClick={() => this.changeNav()} className="fas fa-times" /></div>}
+                    </div>
+                    <nav className="main-nav">
+                        <ul className="main-nav-list">
+                            <li>Home</li>
+                            <li>Solutions &amp; Products</li>
+                            <li>The Tyler Expierence</li>
+                            <li>About Us</li>
+                            <li>Client Support</li>
+                            <li>Careers</li>
+                            <li>Contact Us</li>
+                            <li>Investors</li>
 
-                    {this.state.navActive === false ? <div className="hamburger"><i onClick={() => this.changeNav()} className="fas fa-bars" /></div> :
-                        <div className="hamburger"><i onClick={() => this.changeNav()} className="fas fa-times" /></div>}
+                        </ul>
+                    </nav>
                 </header>
-                {/* <nav className="main-nav">
-                    <ul>
-                        <li>Home</li>
-                        <li>Solutions &amp; Products</li>
-                        <li>The Tyler Expierence</li>
-                        <li>About Us</li>
-                        <li>Client Support</li>
-                        <li>Careers</li>
-                        <li>Contact Us</li>
-                        <li>Investors</li>
-
-                    </ul>
-                </nav> */}
 
                 {this.state.navActive === true ? <nav className="mobile-nav">
                     <input placeholder="Search" />
                     <ul className="mobile-nav-list">
                         <Link to="/">
-                        <li>Home</li>
+                            <li>Home</li>
                         </Link>
                         <Link to="/solutions-products">
-                        <li>Solutions &amp; Products</li>
+                            <li>Solutions &amp; Products</li>
                         </Link>
                         <li>The Tyler Expierence</li>
                         <li>About Us</li>
@@ -57,7 +59,7 @@ class Header extends Component {
                     </ul>
                 </nav>
                     : null}
-         
+
             </div>
         )
     }
